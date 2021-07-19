743461211aa0c4d08732114c02938d660f10555d
Robert Luke <hello@robertluke.net>
2021-07-19 15:55:43 +1000

    Add more tets

--- .github/workflows/tests_script.py
+++ .github/workflows/tests_script.py
@@ -1,4 +1,4 @@
-from mne_bids import get_entity_vals
+from mne_bids import get_entity_vals, BIDSPath, read_raw_bids
 subs = get_entity_vals("/bids_dataset", 'subject')
 print(f"Found subjects: {subs}")
 assert len(subs) == 5
@@ -8,3 +8,14 @@ assert len(sess) == 0
 tasks = get_entity_vals("/bids_dataset", 'task')
 print(f"Found tasks: {tasks}")
 assert len(tasks) == 1
+
+for sub in subs:
+    for task in tasks:
+        for ses in sess:
+            b_path = BIDSPath(subject=sub, task=task, session=ses,
+                              root="/bids_dataset",
+                              datatype="nirs", suffix="nirs",
+                              extension=".snirf")
+            raw = read_raw_bids(b_path, verbose=True)
+            assert "fnirs_cw_amplitude" in raw
+            assert len(raw) > 10
